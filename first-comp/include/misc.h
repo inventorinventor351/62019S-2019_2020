e09cf881fc392923db8e20fe6a34eee006de5e45
Karl Velazquez <karlvelaz350@gmail.com>
2019-06-11 13:38:18 -0700

    ryyrtn

--- first-comp/include/misc.h
+++ first-comp/include/misc.h
@@ -1,6 +1,24 @@
 #ifndef _MISC_H_
 #define _MISC_H_
 
+double sgn(double x);
 
+class PID {
+
+    private:
+        double error,
+        integral,
+        derivative,
+        prevError,
+        kP, kI, kD,
+        output;
+        bool integrate;
+
+    public:
+        PID(double _kP, double _kI, double _kD);
+        void setError(double _error);
+        double runPID();
+
+};
 
 #endif
--- first-comp/src/misc.cpp
+++ first-comp/src/misc.cpp
@@ -1,1 +1,44 @@
 #include "main.h"
+
+double sgn(double x) {
+
+    return (x > 0) - (x < 0);
+
+}
+
+PID::PID(double _kP, double _kI, double _kD) {
+
+    kP = _kP;
+    kI = _kI;
+    kD = _kD;
+
+}
+
+void PID::setError(double _error) {
+
+    error = _error;
+
+}
+
+double PID::runPID() {
+
+    integral += integrate ? error : 0;
+    derivative = error - prevError;
+    prevError = error;
+    output = (kP * error) + (kI * integral) + (kD* derivative);
+    
+    if(abs(output) > 100) {
+
+        integrate = (sgn(error) == sgn(output)) ? 0 : 1;
+        return 100 * sgn(output);
+
+    }
+
+    else {
+
+        integrate = 1;
+        return output;
+
+    }
+
+}
