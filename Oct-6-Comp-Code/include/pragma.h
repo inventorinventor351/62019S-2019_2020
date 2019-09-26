64f5f6ffda4523921a1ce7d371ac26634cfc0f27
Karl Velazquez <karlvelaz350@gmail.com>
2019-09-26 14:24:36 -0700

    added pragma

---
+++ Oct-6-Comp-Code/include/pragma.h
@@ -0,0 +1,14 @@
+#ifndef _PRAGMA_H_
+#define _PRAGMA_H_
+
+extern Controller master;
+
+extern Motor leftBase1;
+extern Motor leftBase2;
+extern Motor rightBase1;
+extern Motor rightBase2;
+extern Motor intake1;
+extern Motor intake2;
+extern Motor tray;
+
+#endif
