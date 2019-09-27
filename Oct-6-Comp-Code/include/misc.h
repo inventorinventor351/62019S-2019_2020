1a1cd34caa646da7c33cbb747788e6c2635c8307
Karl Velazquez <karlvelaz350@gmail.com>
2019-09-26 18:23:13 -0700

    added sign function and started PID class API

--- Oct-6-Comp-Code/include/misc.h
+++ Oct-6-Comp-Code/include/misc.h
@@ -1,6 +1,25 @@
 #ifndef _MSIC_H_
 #define _MISC_H_
 
+double sgn(double x);
 
+class PID {
+
+    private:
+        double error,
+        integral,
+        derivative,
+        prevError,
+        kP, kI, kD,
+        output;
+        bool integrate;
+
+    public:
+        PID(double _kP, double _kI, double _kD);
+        double getError();
+        void setError(double _error);
+        double runPID();
+
+};
 
 #endif
