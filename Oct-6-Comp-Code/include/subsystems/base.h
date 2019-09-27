9d73854fdcf9cb5906990e54a2e667afbab24fd3
Karl Velazquez <karlvelaz350@gmail.com>
2019-09-26 18:32:14 -0700

    created driver control

--- Oct-6-Comp-Code/include/subsystems/base.h
+++ Oct-6-Comp-Code/include/subsystems/base.h
@@ -1,7 +1,7 @@
 #ifndef _BASE_H_
 #define _BASE_H_
 
-void runleftBase(double percVolt);
-void runrightBase(double percVolt);
+void runLeftBase(double percVolt);
+void runRightBase(double percVolt);
 
 #endif
--- Oct-6-Comp-Code/src/main.cpp
+++ Oct-6-Comp-Code/src/main.cpp
@@ -29,7 +29,26 @@ void opcontrol() {
 
 	while (true) {
 
+		runLeftBase(master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) * 100 / 127);
+		runRightBase(master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y) * 100 / 127);
 
+		if(master.get_digital(E_CONTROLLER_DIGITAL_R1))
+			runIntk(100);
+
+		else if(master.get_digital(E_CONTROLLER_DIGITAL_R2))
+			runIntk(-100);
+
+		else
+			runIntk(0);
+
+		if(master.get_digital(E_CONTROLLER_DIGITAL_L1))
+			runTray(100);
+
+		else if(master.get_digital(E_CONTROLLER_DIGITAL_L2))
+			runTray(-100);
+
+		else
+			runTray(0);
 
 	}
 
