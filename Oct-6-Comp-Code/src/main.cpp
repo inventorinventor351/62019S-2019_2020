863425c62b05881f00b9487d34a3bf182ed17691
chris salas <topher1883@gmail.com>
2019-09-30 14:28:03 -0700

    mht

--- Oct-6-Comp-Code/src/main.cpp
+++ Oct-6-Comp-Code/src/main.cpp
@@ -21,78 +21,8 @@ void initialize() {
 
 
 
-int autonCount = 0;
-
-void lcdScroll() {
-
-    if(autonCount < 0) {
-
-        autonCount = 3;
-
-    }
-
-    else if(autonCount > 3) {
-
-        autonCount = 0;
-
-    }
-
-    switch(autonCount) {
-
-            case 0:
-                lcd::set_text(1, "BLUE Big Zone");
-                break;
-
-            case 1:
-                lcd::set_text(1, "BLUE Small Zone");
-                break;
-
-            case 2:
-                lcd::set_text(1, "RED Big Zone");
-                break;
-
-            case 3:
-                lcd::set_text(1, "RED Small Zone");
-                break;
-
-        }
-
-}
-
-void on_left_pressed() {
-
-    autonCount--;
-    lcdScroll();
-
-}
-
-void on_center_pressed() {
-
-    autonCount = autonCount;
-    lcd::shutdown();
-
-}
-
-void on_right_pressed() {
-
-    autonCount++;
-    lcdScroll();
-
-}
-
-
-
-
-
 void competition_initialize() {
     
-	lcd::initialize();
-    lcd::set_text(0, "choose auton");
-    lcdScroll();
-    lcd::register_btn0_cb(on_left_pressed);
-    lcd::register_btn1_cb(on_center_pressed);
-    lcd::register_btn2_cb(on_right_pressed);
-
 
 }
 
