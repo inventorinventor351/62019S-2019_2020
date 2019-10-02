1df2a3e62cf42c9a6903a96e3b11f58fb273cfe0
chris salas <topher1883@gmail.com>
2019-10-02 13:39:57 -0700

    blue small

--- Oct-6-Comp-Code/src/autons/blueSmallZone.cpp
+++ Oct-6-Comp-Code/src/autons/blueSmallZone.cpp
@@ -3,18 +3,15 @@
 void blueSmallZone() 
 {
     runIntk(40);
-    delay(500);
-    moveBase(100, 100, 1500);
-    delay(500);
+    moveBase(1100, 1100, 1500);
     runIntk(0);
-    moveBase(100, 50, 500);
-    delay(100);
-    runIntk(50);
-    moveBase(20, 20, 500);
-    
+    moveBase(-500, -500, 1000);
+    moveBase(100, -500, 1000);
+    moveBase(1000, 1000, 1000);
+    runTray(-50);
+    delay(200);
+    runIntk(20);
+    moveBase(-100, -100, 2000);
 
-
-
-    
 
 }
