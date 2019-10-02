1ad5499f517243b62a11588756c82aac0b17cbd3
Karl Velazquez <karlvelaz350@gmail.com>
2019-10-02 13:36:37 -0700

    fixed some LCD shit

--- Oct-6-Comp-Code/src/main.cpp
+++ Oct-6-Comp-Code/src/main.cpp
@@ -18,80 +18,8 @@ void initialize() {
 
 }
 
-
-
-
-int autonCount = 0;
-
-void lcdScroll() {
-
-    if(autonCount < 0) {
-
-        autonCount = 3;
-
-    }
-
-    else if(autonCount > 3) {
-
-        autonCount = 0;
-
-    }
-
-    switch(autonCount) {
-
-            case 0:
-                lcd::set_text(1, "BLUE Big Zone");
-                break;
-
-            case 1:
-                lcd::set_text(1, "BLUE Small Zone");
-                break;
-
-            case 2:
-                lcd::set_text(1, "RED Big Zone");
-                break;
-
-            case 3:
-                lcd::set_text(1, "RED Small Zone");
-                break;
-
-        }
-
-}
-
-void on_left_pressed() {
-
-    autonCount--;
-    lcdScroll();
-
-}
-
-void on_center_pressed() {
-
-    autonCount = autonCount;
-    lcd::shutdown();
-
-}
-
-void on_right_pressed() {
-
-    autonCount++;
-    lcdScroll();
-
-}
-
-
-
-
-
 void competition_initialize() {
-    
-	lcd::initialize();
-    lcd::set_text(0, "choose auton");
-    lcdScroll();
-    lcd::register_btn0_cb(on_left_pressed);
-    lcd::register_btn1_cb(on_center_pressed);
-    lcd::register_btn2_cb(on_right_pressed);
+
 
 
 }
@@ -131,11 +59,21 @@ void opcontrol() {
 
 		}
 
-		else if(master.get_digital(E_CONTROLLER_DIGITAL_L2))
+		else if(master.get_digital(E_CONTROLLER_DIGITAL_L2)) {
+
+			intake1.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+			intake2.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 			runTray(75);
 
-		else
+		}
+
+		else {
+
+			intake1.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+			intake2.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 			runTray(0);
+
+		}
 
 	}
 
