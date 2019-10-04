f7e637eb3ff360826444981a4e7f00f394a17251
chris salas <topher1883@gmail.com>
2019-10-03 17:39:11 -0700

    th

--- Oct-6-Comp-Code/src/autons/blueBigZone.cpp
+++ Oct-6-Comp-Code/src/autons/blueBigZone.cpp
@@ -2,7 +2,33 @@
 
 void blueBigZone() {
 
-    runIntk(85); //run intake
+    
+    runIntk(80);
+    delay(500);
+    moveBase(1100, 1100, 2000, 100);
+    delay(100);
+    runIntk(0);
+    moveBase(-500, -500, 1000, 100);
+    moveBase(200, -200, 1000, 80);
+    moveBase(500, 500, 1500, 80);
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*runIntk(85); //run intake
     //need to somehow open up the tray and anti tip cause they passive
     moveBase(520, 520, 1000, 1000); //move forward, pick up cube
     runIntk(0); //stop intake otherwise cube might fly out of tray
@@ -23,6 +49,6 @@ void blueBigZone() {
     //move forward, pick up those three cubes previously left behind
     //move forward up to goal zone border thing
     //push tray forward at some speed that doesnt knock the 7 cubes down
-    //move backward
+    //move backward*/
 
 }
