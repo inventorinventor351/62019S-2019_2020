032e5d92e0fb206838c8efd597425e519468aadd
chris salas <topher1883@gmail.com>
2019-10-20 14:49:10 -0700

    red big zone

--- Oct-26-Comp-Code/src/autons/redBigZone.cpp
+++ Oct-26-Comp-Code/src/autons/redBigZone.cpp
@@ -2,5 +2,20 @@
 
 void redigZone()
 {
-    
+
+
+    runIntk(80);
+    delay(500);
+    moveBase(1100, 1100, 2000, 100);
+    delay(100);
+    runIntk(0);
+    moveBase(-500, -500, 1000, 100);
+    moveBase(-200, 200, 1000, 80);
+    moveBase(500, 500, 1500, 80);
+    runTray(-50);
+    delay(200);
+    runIntk(20);
+    moveBase(-100, -100, 2000, 100);
+
+
 }
