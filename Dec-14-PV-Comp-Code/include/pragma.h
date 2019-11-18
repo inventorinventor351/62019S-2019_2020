bb8b56dfe49e3257e78f740d43b8a7c80b5874fa
Karl Velazquez <karlvelaz350@gmail.com>
2019-11-18 11:41:14 -0800

    finished pragma

--- Dec-14-PV-Comp-Code/include/pragma.h
+++ Dec-14-PV-Comp-Code/include/pragma.h
@@ -1,6 +1,21 @@
 #ifndef _PRAGMA_H_
 #define _PRAGMA_H_
 
+extern Controller master;
 
+extern ADIEncoder leftEnc;
+extern ADIEncoder rightEnc;
+extern ADIEncoder yawEnc;
+extern ADIAnalogIn trayPot;
+extern ADIAnalogIn armPot;
+
+extern Motor leftBase1;
+extern Motor leftBase2;
+extern Motor rightBase1;
+extern Motor rightBase2;
+extern Motor intake1;
+extern Motor intake2;
+extern Motor tray;
+extern Motor arm;
 
 #endif
--- Dec-14-PV-Comp-Code/src/pragma.cpp
+++ Dec-14-PV-Comp-Code/src/pragma.cpp
@@ -1,1 +1,18 @@
 #include "main.h"
+
+Controller master (E_CONTROLLER_MASTER);
+
+ADIEncoder leftEnc (1, 2, 0);
+ADIEncoder rightEnc (3, 4, 1);
+ADIEncoder yawEnc (5, 6, 0);
+ADIAnalogIn trayPot (7);
+ADIAnalogIn armPot (8);
+
+Motor leftBase1 (1, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_COUNTS);
+Motor leftBase2 (2, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_COUNTS);
+Motor rightBase1 (3, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_COUNTS);
+Motor rightBase2 (4, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_COUNTS);
+Motor intake1 (5, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_COUNTS);
+Motor intake2 (6, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_COUNTS);
+Motor tray (7, E_MOTOR_GEARSET_06, 0, E_MOTOR_ENCODER_COUNTS);
+Motor arm (8, E_MOTOR_GEARSET_06, 0, E_MOTOR_ENCODER_COUNTS);
