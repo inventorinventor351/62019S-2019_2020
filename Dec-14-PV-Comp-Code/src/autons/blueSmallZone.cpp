7cbc6e78490a6b9c36ea36d8faad585152f7d9aa
Karl Velazquez <karlvelaz350@gmail.com>
2019-12-13 17:34:11 -0800

    i think i finished the autons

--- Dec-14-PV-Comp-Code/src/autons/blueSmallZone.cpp
+++ Dec-14-PV-Comp-Code/src/autons/blueSmallZone.cpp
@@ -29,26 +29,34 @@ void blueSmallZone() {
     runRollers(0);
     moveBase(-2700, -1200, 3000, 100);*/
 
+    armsActive = false;
     trayTarget = TRAY_OUT_OF_THE_WAY;
-    moveBase(1000, 1000, 775, 100);
+    moveBase(800, 800, 500, 100);
     runRollers(-100);
-    armsTarget = ARMS_FULLY_UP;
-    delay(400);
+    runArms(100);
+    delay(550);
+    armsActive = true;
     armsTarget = ARMS_FULLY_DOWN;
-    runLeftBase(-50);
-    runRightBase(-50);
-    delay(250);
-    runLeftBase(0);
-    runRightBase(0);
-    runRollers(100);
     trayTarget = TRAY_FULLY_IN;
-    moveBase(3500, 2700, 4000, 40);
-    runRollers(0);
-    moveBase(-3925, -3250, 2000, 100);
-    moveBase(800, 800, 600, 100);
+    delay(150);
     runRollers(100);
-    moveBase(2800, 2800, 4000, 30);
+    moveBase(3100, 2400, 3200, 40);
     runRollers(0);
-    moveBase(-2700, -600, 3000, 100);
+    moveBase(-3625, -2900, 1500, 100);
+    runRollers(100);
+    moveBase(3150, 3150, 3450, 32.5);
+    runRollers(15);
+    moveBase(-2850, -450, 1750, 100);
+    runRollers(0);
+    moveBase(2000, 2250, 1600, 100);
+    trayTarget = TRAY_FULLY_OUT;
+    delay(1800);
+    runLeftBase(40);
+    runRightBase(40);
+    delay(200);
+    runLeftBase(-100);
+    runRightBase(-100);
+    delay(300);
+    trayTarget = TRAY_FULLY_IN;
     
 }
