5be1c751cfd0ce3030ba153ad7c6978fbb11fc65
Karl Velazquez <karlvelaz350@gmail.com>
2019-12-17 11:42:47 -0800

    declared +defined all robot components in pragma

--- dr4b-shlong/include/pragma.h
+++ dr4b-shlong/include/pragma.h
@@ -1,6 +1,22 @@
 #ifndef _PRAGMA_H_
 #define _PRAGMA_H_
 
+extern Controller master; //main and only controller
 
+/* LEGACY COMPONENTS */
+extern ADIEncoder leftEnc; //encoder on the left tracker wheel
+extern ADIEncoder rightEnc; //encoder on the right tracker wheel
+extern ADIAnalogIn trayPot; //potentiometer on the tray
+extern ADIAnalogIn liftPot; //potentionmeter on the dr4b lift
+
+/* V5 COMPONENTS */
+extern Motor leftBase1; //front left base motor
+extern Motor leftBase2; //back left base motor
+extern Motor rightBase1; //front right base motor
+extern Motor rightBase2; //back right base motor
+extern Motor leftRoller; //left roller motor
+extern Motor rightRoller; //right roller motor
+extern Motor trayMtr; //motor that tils the tray
+extern Motor liftMtr; //motor that runs the dr4b lift up and down
 
 #endif
--- dr4b-shlong/src/pragma.cpp
+++ dr4b-shlong/src/pragma.cpp
@@ -1,1 +1,17 @@
 #include "main.h"
+
+Controller master (E_CONTROLLER_MASTER); //initializing main and only controller as the master controller
+
+ADIEncoder leftEnc (1, 2, 0); //legacy ports A and B, not reversed
+ADIEncoder rightEnc (3, 4, 1); //legacy ports C and D, reversed
+ADIAnalogIn trayPot (5); //legacy port E
+ADIAnalogIn liftPot (6); //legacy port F
+
+Motor leftBase1 (1, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_COUNTS); //v5 port 1, 200 rpm, not reversed, returns its encoder value in raw untis
+Motor leftBase2 (2, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_COUNTS); //v5 port 2, 200 rpm, not reversed, returns its encoder value in raw untis
+Motor rightBase1 (3, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_COUNTS); //v5 port 3, 200 rpm, reversed, returns its encoder value in raw untis
+Motor rightBase2 (4, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_COUNTS); //v5 port 4, 200 rpm, reversed, returns its encoder value in raw untis
+Motor leftRoller (5, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_COUNTS); //v5 port 5, 200 rpm, not reversed, returns its encoder value in raw untis
+Motor rightRoller (6, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_COUNTS); //v5 port 6, 200 rpm, reversed, returns its encoder value in raw untis
+Motor trayMtr (7, E_MOTOR_GEARSET_36, 0, E_MOTOR_ENCODER_COUNTS); //v5 port 7, 100 rpm, not reversed, returns its encoder value in raw untis
+Motor liftMtr (8, E_MOTOR_GEARSET_36, 0, E_MOTOR_ENCODER_COUNTS); //v5 port 8, 100 rpm, not reversed, returns its encoder value in raw untis
