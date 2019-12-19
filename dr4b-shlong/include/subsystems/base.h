7acafbec8296e9f9b2b6fc0f7d331808b0096d3d
Karl Velazquez <karlvelaz350@gmail.com>
2019-12-19 12:17:33 -0800

    edited some comments

--- dr4b-shlong/include/subsystems/base.h
+++ dr4b-shlong/include/subsystems/base.h
@@ -3,10 +3,10 @@
 
 void pwrLeftBase(double voltPerc); //directly run voltage into left base motors, takes in a percentage from -100 to 100
 void pwrRightBase(double voltPerc); //directly run voltage into right base motors, takes in a percentage from -100 to 100
-double getLeftVel(void* param); //task that calculates rpm for left side of base
-double getRightVel(void* param); //task that calculates rpm for right side of base
-extern double leftVelTarget; //left target rpm that the FPID controller for the base will try to meet
-extern double rightVelTarget; //right target rpm that the FPID controller for the base will try to meet
+double getLeftVel(void* param); //task that calculates rpm for left side of base (idk if its units are rotations per minute tho)
+double getRightVel(void* param); //task that calculates rpm for right side of base (idk if its units are rotations per minute tho)
+extern double leftVelTarget; //left target rpm that the FPID controller for the base will try to meet (idk if its units are rotations per minute tho)
+extern double rightVelTarget; //right target rpm that the FPID controller for the base will try to meet (idk if its units are rotations per minute tho)
 void baseVelControl(void* param); //FPID base controller task
 
 #endif
