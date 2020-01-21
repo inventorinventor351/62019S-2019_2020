e70a51ed56b47bba90e26aae67449f991cbd668e
Karl Velazquez <karlvelaz350@gmail.com>
2020-01-21 11:58:59 -0800

    started deploy sequence

--- shlong+arms/include/autonRoutines.h
+++ shlong+arms/include/autonRoutines.h
@@ -1,6 +1,6 @@
 #ifndef _AUTON_ROUTINES_H_
 #define _AUTON_ROUTINES_H_
 
-
+void deploy(); //deploys tray and anti tip at start of auton
 
 #endif
--- shlong+arms/src/autonomous routines/deploy.cpp
+++ shlong+arms/src/autonomous routines/deploy.cpp
@@ -1,1 +1,16 @@
 #include "main.h"
+
+void deploy() {
+
+    armsTaskActive = false;
+    trayTarget = TRAY_OUT_OF_THE_WAY;
+    driveStraight(800, 500, 100);
+    pwrRollers(-100);
+    pwrArms(100);
+    betterDelay(500);
+    armsTaskActive = true;
+    armsTarget = ARMS_FULLY_DOWN;
+    trayTarget = TRAY_FULLY_IN;
+    betterDelay(150);
+
+}
