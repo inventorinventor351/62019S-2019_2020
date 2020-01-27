c270d6c2a99272230a91ed6521d4cd07b44d3c2d
Karl Velazquez <karlvelaz350@gmail.com>
2020-01-27 11:44:05 -0800

    renamed file and folder

--- dr4b-shlong/include/autonRoutines.h
+++
@@ -1,6 +0,0 @@
-#ifndef _AUTON_ROUTINES_H_
-#define _AUTON_ROUTINES_H_
-
-
-
-#endif
---
+++ dr4b-shlong/include/autons.h
@@ -0,0 +1,10 @@
+#ifndef _AUTON_ROUTINES_H_
+#define _AUTON_ROUTINES_H_
+
+void deploy(); //a sequence of actions that deploys tray and rollers
+void blueBigZone(); //8pt auton for blue protected zone
+void blueSmallZone(); //8pt auton for blue unprotected zone
+void redBigZone(); //8pt auton for red protected zone
+void redSmallZone(); //8pt auton for red unprotected zone
+
+#endif
