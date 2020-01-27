91afc7b039e03c55d9ef52fc8601f9dce8c9c863
Karl Velazquez <karlvelaz350@gmail.com>
2020-01-27 11:21:14 -0800

    added to header files of tray and lift

--- dr4b-shlong/include/subsystems/lift.h
+++ dr4b-shlong/include/subsystems/lift.h
@@ -2,5 +2,10 @@
 #define _LIFT_H_
 
 void pwrLift(double voltPerc); //directly run voltage into lift motor, takes in a percentage from -100 to 100
+#define LIFT_FULLY_DOWN 0 //lift potentiomter value when lift is all the way down
+#define LIFT_FULL_UP 1000 //lift potentiometer value when lift is all the way up
+extern double liftTarget; //lift task minimizes difference between lift potentiometer and this value
+extern bool liftTaskActive; //activates or deactives the lift task function
+void liftTask(void* param); //PID control that gets the lift to specific positions
 
 #endif
--- dr4b-shlong/include/subsystems/tray.h
+++ dr4b-shlong/include/subsystems/tray.h
@@ -2,5 +2,10 @@
 #define _TRAY_H_
 
 void pwrTray(double voltPerc); //directly run voltage into tray motor, takes in a percentage from -100 to 100
+#define TRAY_FULLY_DOWN 0 //tray potentiomter value when tray is all the way down
+#define TRAY_FULL_UP 1000 //tray potentiometer value when tray is all the way up
+extern double trayTarget; //tray task minimizes difference between tray potentiometer and this value
+extern bool trayTaskActive; //activates or deactives the tray task function
+void trayTask(void* param); //PID control that gets the tray to specific positions
 
 #endif
