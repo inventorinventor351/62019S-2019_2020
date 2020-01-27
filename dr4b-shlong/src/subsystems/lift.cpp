66e62fd48b2864bf30160eac7f34ced8ea998e5f
Karl Velazquez <karlvelaz350@gmail.com>
2020-01-27 11:32:26 -0800

    added PID contorl to lift and tray source files

--- dr4b-shlong/src/subsystems/lift.cpp
+++ dr4b-shlong/src/subsystems/lift.cpp
@@ -5,3 +5,29 @@ void pwrLift(double voltPerc) {
     liftMtr.move_voltage(voltPerc * 12000 / 100); //divided percentage by 100 to get ratio and multiply ratio with 12000
 
 }
+
+double liftTarget = LIFT_FULLY_DOWN; //lift should stay down at start of program execution
+bool liftTaskActive = true; //lift task should be active at start of program execution
+
+void liftTask(void* param) {
+
+    std::uint_least32_t now = millis();
+
+    FPID liftPID (0, 0, 0, 0); //kF = 0, kP = 0, kI = 0, kD = 0
+    liftPID.setOutputBounds(0, 100); //minOutput = 0%, maxOutput = 100%
+
+    while(true) { //loop indefinitely
+
+        if(liftTaskActive) { //if liftTaskActive equals true, then run code inside brackets
+
+            liftPID.setSetPoint(liftTarget); //set setpoint to the lift target specified by user
+            liftPID.setSystemVar(liftPot.get_value()); //set system variable to the lift potentiometer
+            pwrLift(liftPID.run()); //run FPID algorithm using variables specified above and power the lift motor to the output
+
+        }
+
+        Task::delay_until(&now, 10); //iterate 100 times a second
+
+    }
+
+}
--- dr4b-shlong/src/subsystems/tray.cpp
+++ dr4b-shlong/src/subsystems/tray.cpp
@@ -5,3 +5,29 @@ void pwrTray(double voltPerc) {
     trayMtr.move_voltage(voltPerc * 12000 / 100); //divided percentage by 100 to get ratio and multiply ratio with 12000
 
 }
+
+double trayTarget = TRAY_FULLY_DOWN; //tray should stay down at start of program execution
+bool trayTaskActive = true; //tray task should be active at start of program execution
+
+void trayTask(void* param) {
+
+    std::uint_least32_t now = millis();
+
+    FPID trayPID (0, 0, 0, 0); //kF = 0, kP = 0, kI = 0, kD = 0
+    trayPID.setOutputBounds(0, 100); //minOutput = 0%, maxOutput = 100%
+
+    while(true) { //loop indefinitely
+
+        if(trayTaskActive) { //if trayTaskActive equals true, then run code inside brackets
+
+            trayPID.setSetPoint(trayTarget); //set setpoint to the tray target specified by user
+            trayPID.setSystemVar(trayPot.get_value()); //set system variable to the tray potentiometer
+            pwrTray(trayPID.run()); //run FPID algorithm using variables specified above and power the tray motor to the output
+
+        }
+
+        Task::delay_until(&now, 10); //iterate 100 times a second
+
+    }
+
+}
