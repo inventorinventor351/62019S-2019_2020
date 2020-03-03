53a7cd79c77473c580af1af9038347a237a5d1f4
Karl Velazquez <karlvelaz350@gmail.com>
2020-03-02 16:33:15 -0800

    worked on blue small zone auton

--- dr4b-shlong/src/autons/blueSmallZone.cpp
+++ dr4b-shlong/src/autons/blueSmallZone.cpp
@@ -6,34 +6,33 @@ void blueSmallZone() {
     pwrRollers(100);
     liftTaskActive = false;
     pwrLift(100);
-    basePivot(0, 300, 90);
+    basePivot(0, 275, 90);
     pwrLift(-100);
-    basePivot(0, 300, 90);
+    basePivot(0, 275, 90);
     pwrLift(0);
+    basePivot(0, 100, 90);
 
     //collect first two
     driveStraight(1150, 700, 90);
-    driveStraight(900, 1800, 25);
-    pwrRollers(0);
+    driveStraight(900, 1700, 25);
 
     //turn, go back, and face the four cubes
     basePivot(550, 900, 90);
-    driveStraight(-2425, 1300, 90);
-    basePivot(-575, 1000, 90);
-    pwrRollers(100);
+    driveStraight(-2500, 1400, 90);
+    basePivot(-600, 1000, 90);
 
     //collect last four
-    driveStraight(2500, 2400, 45);
+    driveStraight(2500, 2200, 45);
 
     //turn and go to goal
     driveStraight(-1575, 1300, 90);
+    basePivot(-1175, 1350, 90);
+    driveStraight(950, 1000, 90);
     pwrRollers(0);
-    basePivot(-1170, 1300, 90);
-    driveStraight(1200, 1000, 90);
 
     //stack
     trayTarget = TRAY_FULL_UP;
-    basePivot(0, 2000, 90);
+    basePivot(0, 2250, 90);
 
     //pull out
     pwrLeftBase(-100);
--- dr4b-shlong/src/main.cpp
+++ dr4b-shlong/src/main.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int autonCount = 0;
+int autonCount = 2;
 
 double sgn(double x) { //returns sign (-1 or 1) of a number, and 0 for 0
 
